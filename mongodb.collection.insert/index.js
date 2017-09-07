35be2c87f2bf5d259d7ae9e3de54a780ad3c091b
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-09-07 20:56:24 +0200

    linter fixes

--- mongodb.collection.insert/index.js
+++ mongodb.collection.insert/index.js
@@ -16,6 +16,7 @@ module.exports = (NODE) => {
     .then(([collections, documents]) =>
       // loop the collections and insert
       Promise.all(collections.map(collection => collection.insertMany(documents)))
-    ).then(() => doneOut.trigger(state));
+    )
+    .then(() => doneOut.trigger(state));
   });
 };
