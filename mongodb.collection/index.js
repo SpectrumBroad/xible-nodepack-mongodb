04723b7fed6667772a3c84a7460a3ed95458ed26
Jong <Stefan.de.Jong.wia@ing.com>
2019-03-09 09:57:55 +0100

    async/await and improved error handling

--- mongodb.collection/index.js
+++ mongodb.collection/index.js
@@ -6,30 +6,30 @@ module.exports = (NODE) => {
   const collectionsOut = NODE.getOutputByName('collections');
   const docsOut = NODE.getOutputByName('documents');
 
-  collectionsOut.on('trigger', (conn, state, callback) => {
+  collectionsOut.on('trigger', async (conn, state) => {
     if (!mongosIn.isConnected()) {
       return;
     }
 
-    mongosIn.getValues(state)
-    .then((mongos) => {
-      callback(mongos.map(mongo => mongo.collection(NODE.data.collectionName)));
-    });
+    const mongos = await mongosIn.getValues(state);
+    return mongos.map(mongo => mongo.collection(NODE.data.collectionName));
   });
 
-  docsOut.on('trigger', (conn, state, callback) => {
+  docsOut.on('trigger', async (conn, state) => {
     if (!mongosIn.isConnected()) {
       return;
     }
 
-    mongosIn.getValues(state)
-    .then((mongos) => {
-      Promise.all(
+    const mongos = await mongosIn.getValues(state);
+
+    try {
+      const [arrs] = await Promise.all(
         mongos.map(mongo => mongo.collection(NODE.data.collectionName).find().toArray())
-      )
-      .then((arrs) => {
-        callback([].concat(...arrs));
-      });
-    });
+      );
+
+      return [].concat(...arrs);
+    } catch (err) {
+      NODE.error(err, state);
+    }
   });
 };
